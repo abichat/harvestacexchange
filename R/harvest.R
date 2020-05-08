35d3732766f29d5a4c0acf1d8c58dbe10d8ad957
Antoine Bichat <a.bichat@yahoo.fr>
2020-05-08 13:09:02 +0200

    Ask user

--- R/harvest.R
+++ R/harvest.R
@@ -6,6 +6,15 @@
 #' @return \code{NULL}
 #'
 harvest_and_write <- function(page, path_write){
+
+  dep <- dir.exists(system.file("js", "node_modules",
+                                package = "harvestacexchange"))
+
+  if(!dep){
+    stop("You need to install JavaScript libraries with ",
+         "harvestacexchange::install_js_dep() first.")
+  }
+
   path_js <- system.file("js", "write_html.js", package = "harvestacexchange")
   command <- paste("node", path_js, page, path_write)
   system(command)
--- R/js_dep.R
+++ R/js_dep.R
@@ -1,21 +1,40 @@
-#' Install JavaScript libraries and scripts
+#' Install JavaScript libraries
 #'
 #' @details This must be done once per installation. You must have Node.js installed
 #' on your computer.
 #'
+#' @param force Logical. Should the user be prompted to install?
+#'
+#' @importFrom yesno yesno
 #'
 #' @return \code{NULL}
 #' @export
 #'
 #' @examples
 #' \dontrun{install_js_dep()}
-install_js_dep <- function(){
+install_js_dep <- function(force = FALSE){
   if(!check_nodejs()){
     stop("You need to install Node.js on your computer.")
   }
-  dir_js <- system.file("js", package = "harvestacexchange")
-  command <- paste0("npm install --prefix ", dir_js," playwright")
-  system(command)
+
+  if (!force) {
+    if (dir.exists(system.file("js", "node_modules",
+                               package = "harvestacexchange"))) {
+      ok <- yesno("JavaScript libraries are already installed on your ",
+                  "local library, do you want to ovewrite them?")
+    } else {
+      ok <- yesno("This will install JavaScript libraries on your local ",
+                  "library, are you ok with that?")
+    }
+  } else {
+    ok <- TRUE
+  }
+
+  if(ok){
+    dir_js <- system.file("js", package = "harvestacexchange")
+    command <- paste0("npm install --prefix ", dir_js," playwright")
+    system(command)
+  }
   invisible()
 }
 
